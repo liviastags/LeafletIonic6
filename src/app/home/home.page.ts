68a6b3d74929ca46611a085467a4808396be7acf
liviastags <liviastagisellasujarwo@mail.ugm.ac.id>
2024-12-15 02:10:42 +0700

    baru

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,62 +1,241 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import * as L from 'leaflet';
 
 @Component({
-  selector: 'app-home',
-  templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss'],
+  selector: 'app-home', // Nama selector untuk komponen ini
+  templateUrl: './home.page.html', // Lokasi template HTML untuk komponen
+  styleUrls: ['./home.page.scss'], // Lokasi stylesheet untuk komponen
 })
-export class HomePage {
-
+export class HomePage implements OnInit {
   map!: L.Map;
+
+  // Data museum dari JSON yang diberikan
+  museums = [
+    {
+      "Museum": "Jogja National Museum",
+      "Hari": "Monday - Sunday",
+      "Jam": "10.00 am - 10.00 pm",
+      "Harga": "Rp10.000 - Rp20.000",
+      "Rating": "4.6",
+      "latitude": -7.8001175,
+      "longitude": 110.3534164
+    },
+    {
+      "Museum": "Kekayon Museum",
+      "Hari": "Monday - Saturday",
+      "Jam": "08.30 am - 12.00 pm",
+      "Harga": "Rp20.000",
+      "Rating": "4.2",
+      "latitude": -7.8145939,
+      "longitude": 110.4130604
+    },
+    {
+      "Museum": "Menara Benteng Vredeburg",
+      "Hari": "Monday - Sunday",
+      "Jam": "08.00 am - 06.00 pm",
+      "Harga": "Rp15.000 - Rp30.000",
+      "Rating": "5.0",
+      "latitude": -7.8010478,
+      "longitude": 110.3669562
+    },
+    {
+      "Museum": "Museum of Dewantara Kirti Griya",
+      "Hari": "Monday - Saturday",
+      "Jam": "08.00 am - 01.00 pm",
+      "Harga": "Rp3.000",
+      "Rating": "4.7",
+      "latitude": -7.8054438,
+      "longitude": 110.3784719
+    },
+    {
+      "Museum": "AMM Museum Building Yogyakarta",
+      "Hari": "Monday - Saturday",
+      "Jam": "08.00 am - 12.00 am",
+      "Harga": "Free",
+      "Rating": "5.0",
+      "latitude": -7.8300857,
+      "longitude": 110.4030718
+    },
+    {
+      "Museum": "History Of Java Museum (HOJM)",
+      "Hari": "Tuesday - Saturday",
+      "Jam": "09.00 am - 06.00 pm",
+      "Harga": "Rp35.000 - Rp50.000",
+      "Rating": "4.5",
+      "latitude": -7.8439029,
+      "longitude": 110.362261
+    },
+    {
+      "Museum": "Yogyakarta Batik Museum",
+      "Hari": "Monday - Saturday",
+      "Jam": "09.00 am - 03.00 pm",
+      "Harga": "Rp20.000",
+      "Rating": "5.0",
+      "latitude": -7.7956668,
+      "longitude": 110.3772607
+    },
+    {
+      "Museum": "Yogyakarta Palace Batik Museum",
+      "Hari": "Tuesday - Saturday",
+      "Jam": "08.00 am - 02.00 pm",
+      "Harga": "Rp15.000",
+      "Rating": "4.6",
+      "latitude": -7.8075139,
+      "longitude": 110.3645895
+    },
+    {
+      "Museum": "Under the Stairs Educational and Toy Museum",
+      "Hari": "Tuesday - Sunday",
+      "Jam": "08.00 am - 04.00 pm",
+      "Harga": "Rp5.000",
+      "Rating": "5.0",
+      "latitude": -7.8001114,
+      "longitude": 110.3679986
+    },
+    {
+      "Museum": "Museum of Muhammadiyah",
+      "Hari": "Monday - Saturday",
+      "Jam": "09.00 am - 04.00 pm",
+      "Harga": "Rp30.000",
+      "Rating": "4.8",
+      "latitude": -7.8339805,
+      "longitude": 110.3837766
+    },
+    {
+      "Museum": "Central Museum of the Air Force Dirgantara Mandala",
+      "Hari": "Monday - Sunday",
+      "Jam": "08.30 am - 04.00 pm",
+      "Harga": "Free",
+      "Rating": "4.6",
+      "latitude": -7.7903029,
+      "longitude": 110.415762
+    },
+    {
+      "Museum": "Indonesian Women's Movement Museum Mandala Bhakti Wanitatama",
+      "Hari": "Monday - Saturday",
+      "Jam": "08.00 am - 12.00 pm",
+      "Harga": "Rp2.000",
+      "Rating": "4.8",
+      "latitude": -7.7839154,
+      "longitude": 110.3932776
+    },
+    {
+      "Museum": "The Sidik Martowidjojo Museum",
+      "Hari": "Monday - Sunday",
+      "Jam": "10.00 am - 10.00 pm",
+      "Harga": "Free",
+      "Rating": "4.1",
+      "latitude": -7.8084446,
+      "longitude": 110.3629506
+    },
+    {
+      "Museum": "Amri Museum and Gallery",
+      "Hari": "Monday - Sunday",
+      "Jam": "09.00 am - 04.00 pm",
+      "Harga": "Free",
+      "Rating": "4.4",
+      "latitude": -7.7993231,
+      "longitude": 110.3537702
+    },
+    {
+      "Museum": "Tino Sidin Park",
+      "Hari": "Monday - Saturday",
+      "Jam": "09.00 am - 03.00 pm",
+      "Harga": "Rp5.000 - Rp10.000",
+      "Rating": "4.7",
+      "latitude": -7.7995125,
+      "longitude": 110.3449674
+    },
+    {
+      "Museum": "Sonobudaya Museum Unit l",
+      "Hari": "Tuesday - Sunday",
+      "Jam": "08.00 am - 05.00 pm",
+      "Harga": "Rp5.000 - Rp20.000",
+      "Rating": "5.0",
+      "latitude": -7.8024367,
+      "longitude": 110.3643695
+    },
+    {
+      "Museum": "UPY Museorium of History",
+      "Hari": "Monday - Friday",
+      "Jam": "08.00 am - 05.00 pm",
+      "Harga": "Free",
+      "Rating": "4.9",
+      "latitude": -7.8059437,
+      "longitude": 110.3410734
+    },
+    {
+      "Museum": "Kraton Jogja",
+      "Hari": "Tuesday - Sunday",
+      "Jam": "08.00 am - 02.00 pm",
+      "Harga": "Rp10.000 - Rp25.000",
+      "Rating": "4.8",
+      "latitude": -7.8069625,
+      "longitude": 110.3639219
+    },
+    {
+      "Museum": "Yogya Returns Monument",
+      "Hari": "Tuesday - Sunday",
+      "Jam": "08.00 am - 04.00 pm",
+      "Harga": "Rp15.000",
+      "Rating": "4.5",
+      "latitude": -7.7495904,
+      "longitude": 110.3696068
+    },
+    {
+      "Museum": "Omah Jayeng Museum",
+      "Hari": "Tuesday - Sunday",
+      "Jam": "10.00 am - 10.00 pm",
+      "Harga": "Free",
+      "Rating": "4.9",
+      "latitude": -7.7995418,
+      "longitude": 110.3726691
+    },
+    {
+      "Museum": "Omah UGM Kotagede Yogyakarta",
+      "Hari": "Monday - Sunday",
+      "Jam": "09.00 am - 05.00 pm",
+      "Harga": "Free",
+      "Rating": "4.7",
+      "latitude": -7.8293112,
+      "longitude": 110.3956256
+    },
+    {
+      "Museum": "Wijilan Art",
+      "Hari": "Tuesday - Sunday",
+      "Jam": "08.00 am - 08.00 pm",
+      "Harga": "Free",
+      "Rating": "4.5",
+      "latitude": -7.8066064,
+      "longitude": 110.3666953
+    }
+  ];
 
   constructor() {}
 
   ngOnInit() {}
 
-  ionViewDidEnter(){
+  ionViewDidEnter() {
     // Inisialisasi peta
-    this.map = L.map('mapId').setView([35.76943, -5.80081], 10);
+    this.map = L.map('mapId').setView([-7.8001175, 110.3534164], 13);
 
     // Basemap OpenStreetMap
     const openStreetMap = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
       attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
     });
 
-    // Basemap Satellite (ESRI World Imagery)
-    const esriWorldImagery = L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{x}/{y}', {
-      attribution: 'Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community'
-    });
-
-    const grayscale = L.tileLayer('https://{s}.tiles.wmflabs.org/bw-mapnik/{z}/{x}/{y}.png', {
-      attribution: '&copy; OpenStreetMap contributors'
-    });
-
-    const watercolor = L.tileLayer('https://stamen-tiles.a.ssl.fastly.net/watercolor/{z}/{x}/{y}.jpg', {
-      attribution: '&copy; Stamen contributors'
-    });
-
-    const topoMap = L.tileLayer('https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png', {
-      attribution: '&copy; OpenTopoMap contributors'
-    });
-
-    // Menambahkan layer control untuk memilih basemap
-    const baseMaps = {
-      'OpenStreetMap': openStreetMap,
-      'Satellite': esriWorldImagery,
-      'Grayscale': grayscale,
-      'Watercolor': watercolor,
-      'TopoMap': topoMap
-    };
-
-    L.control.layers(baseMaps).addTo(this.map);
-
-    // Set default basemap
+    // Menambahkan layer basemap ke peta
     openStreetMap.addTo(this.map);
 
-    // Menambahkan marker dengan popup informasi
-    const marker = L.marker([35.76943, -5.80081]).addTo(this.map)
-      .bindPopup('<b>Hello world!</b><br>I am a popup.')
-      .openPopup();
+    // Menambahkan marker untuk setiap museum
+    this.museums.forEach(museum => {
+      const marker = L.marker([museum.latitude, museum.longitude]).addTo(this.map)
+        .bindPopup(`
+          <b>${museum.Museum}</b><br>
+          <b>Open Hours:</b> ${museum.Hari} - ${museum.Jam}<br>
+          <b>Price:</b> ${museum.Harga}<br>
+          <b>Rating:</b> ${museum.Rating}<br>
+        `);
+    });
   }
 }
